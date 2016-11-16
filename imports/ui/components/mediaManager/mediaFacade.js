db5876f3a41db0299b37223416760ea89a28a58a
Jordi Campeny Puig <jordicampeny12@gmail.com>
2016-11-16 15:56:41 +0100

    Media resume layout with static images (saved in public/img/pugs). Without DB collections.

--- imports/ui/components/mediaManager/mediaFacade.js
+++ imports/ui/components/mediaManager/mediaFacade.js
@@ -1,12 +1,15 @@
 import angular from 'angular';
 import angularMeteor from 'angular-meteor';
 
+import { name as MediaResume } from './mediaResume/mediaResume';
+
 class MediaFacade {};
 
 const name = 'mediaFacade';
 
 export default angular.module(name, [
-	angularMeteor
+	angularMeteor,
+	MediaResume
 ]).component(name, {
 	controllerAs : name,
 	controller : MediaFacade
---
+++ imports/ui/components/mediaManager/mediaResume/mediaResume.js
@@ -0,0 +1,56 @@
+import angular from 'angular';
+import angularMeteor from 'angular-meteor';
+
+import { Meteor } from 'meteor/meteor';
+
+import template from './mediaResume.html';
+
+class MediaResume{
+	constructor($scope, $reactive){
+		'ngInject';
+
+		$reactive(this).attach($scope);
+
+		this.page = 1;
+		this.perPage = 4;
+
+		// this.helpers({
+		// 	sections(){
+		// 		return Sections.found({
+		// 		}, {
+		// 			limit : parseInt(this.getReactively('perPage')),
+		// 			skip : parseInt((this.getReactively('page') - 1) * this.perPage)
+		// 		});
+		// 	}
+		// });
+		this.sections = [
+			{ _id : 'V2UYB9834YBV43', name : 'Section 1'},
+			{ _id : '4C9M8G34C3MHGY', name : 'Section 2'},
+			{ _id : '84HGU9NRECMW2F', name : 'Section 3'},
+			{ _id : '2H48G934VB3434', name : 'Section 4'}
+		];
+
+		this.medias = [
+			{ _id : 'V2UYB9834YBV43', name : 'Media 1', src: '/img/pugs/pug-1.jpg'},
+			{ _id : '4C9M8G34C3MHGY', name : 'Media 2', src: '/img/pugs/pug-2.jpg'},
+			{ _id : '84HGU9NRECMW2F', name : 'Media 3', src: '/img/pugs/pug-3.jpg'},
+			{ _id : '2H48G934VB3434', name : 'Media 4', src: '/img/pugs/pug-4.jpg'},
+			{ _id : 'V2UYB9834YBV43', name : 'Media 5', src: '/img/pugs/pug-5.jpg'},
+			{ _id : '4C9M8G34C3MHGY', name : 'Media 6', src: '/img/pugs/pug-6.jpg'},
+			{ _id : '84HGU9NRECMW2F', name : 'Media 7', src: '/img/pugs/pug-7.jpg'},
+			{ _id : '2H48G934VB3434', name : 'Media 8', src: '/img/pugs/pug-8.jpg'},
+			{ _id : '2H48G934VB3434', name : 'Media 9', src: '/img/pugs/pug-9.jpg'}
+		];
+		this.totalPages = 10;
+	}
+}
+
+const name = 'mediaResume';
+
+export default angular.module(name, [
+	angularMeteor
+]).component(name, {
+	template,
+	controllerAs : name,
+	controller : MediaResume
+});
