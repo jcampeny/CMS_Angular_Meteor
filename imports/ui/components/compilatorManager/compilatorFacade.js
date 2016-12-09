52a08aa6382778c8b521ef88ebfd38deabd43584
Jordi Campeny Puig <jordicampeny12@gmail.com>
2016-12-09 11:05:32 +0100

    Frontend preparations for the compiler

--- imports/ui/components/compilatorManager/compilatorFacade.js
+++ imports/ui/components/compilatorManager/compilatorFacade.js
@@ -1,7 +1,94 @@
 import angular from 'angular';
 import angularMeteor from 'angular-meteor';
 
+import { Meteor } from 'meteor/meteor';
+import { States } from '../../../api/states';
+import { Pages }  from '../../../api/pages';
+
 class CompilatorFacade {};
+
+class CompilatorObject {
+	constructor (state, pages) {
+		return {
+			_id    : state._id,
+			owner  : Meteor.userId(),
+			states : this.createObject(state, pages)
+		};
+	}
+
+	static getIdsPages (states) {
+		return states.reduce( (result, current) => {
+			result.push(current.id);
+			if (current.states && current.states.length > 0) {
+				result.push(...this.getIdsPages(current.states));
+			}
+			return result;
+		}, []);
+	}
+
+	createObject (state, pages) {
+		return state.states.reduce( (result, current) => {
+			current.page = pages.filter( page => page._id == current.id)[0];
+			
+			if (current.states && current.states.length > 0) {
+				current.states = this.createObject(current, pages);
+			}
+
+			result.push(current);
+
+			return result;
+		}, []);
+	}
+}
+
+class CompilatorFacadeService {
+	constructor($reactive, $rootScope){
+		'ngInject';
+
+		$reactive(this).attach($rootScope);
+		
+		this.root 	  = $rootScope;
+		this.pagesIds = [];
+
+		this.subscribe('states');
+		this.subscribe('pages');
+		
+		this.helpers({
+			state (){
+				return States.findOne({
+					owner : Meteor.userId()
+				});
+			},
+			pages () {
+				return Pages.find({
+					_id : {
+						$in : this.getReactively('pagesIds')
+					}
+				})
+			}
+		});
+
+		this.root.$watch(
+			() => this.pages.length,
+			() => {
+				if (this.pages.length > 0) this.sendToBuild();	
+			}
+		);
+	}
+
+	compile () {
+		this.pagesIds = CompilatorObject.getIdsPages(this.state.states);
+		if (this.pages.length > 0) this.sendToBuild();
+	}
+
+	sendToBuild (state = this.state, pages = this.pages) {
+		let compilatorObj = new CompilatorObject(state, pages);
+
+		this.call('compilator', compilatorObj, 
+			(err,res) => console.log(err,res)
+		);
+	}
+};
 
 const name = 'compilatorFacade';
 
@@ -10,4 +97,4 @@ export default angular.module(name, [
 ]).component(name, {
 	controllerAs : name,
 	controller : CompilatorFacade
-});
+}).service(name, CompilatorFacadeService);
